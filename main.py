99d4a2fc5c7cdf5d5a878a060b1323c99dfea9c5
cYph3r1337 <rishabhcodershukla@gmail.com>
2020-05-23 00:33:06 +0530

    v1.0

---
+++ main.py
@@ -0,0 +1,55 @@
+import time,sys,argparse,datetime,os.path
+from huepy import *
+from concurrent.futures import ThreadPoolExecutor,wait,as_completed
+from src.utitlis import req as request,get_old_data,update_file,Compare_and_add,collect,output_save,date_stamp
+
+parser = argparse.ArgumentParser(description='This is a monitoring tool, it checks for any change in website')
+parser.add_argument('-f', action="store",dest="file",help="file containing urls to monitor")
+parser.add_argument('-t', action="store",dest="t",help="No. of threads", type=int)
+parser.add_argument('-o', action="store",dest="o",help="Directory in which output should be saved")
+input = parser.parse_args()
+
+start_time = time.time()
+diff=[]
+
+url_file_path = input.file
+thread = input.t
+outDir = (input.o or "./")
+output_file_name = os.path.join(outDir, "output.html")
+# fetch old data from data.json
+data= get_old_data()
+futures,html=[],""
+urls = open(url_file_path).read().split('\n')
+print(f'{green("[~]")} Read All Urls Successfully')
+
+executor = ThreadPoolExecutor(max_workers=thread)
+futures = [executor.submit(collect,url,data,diff) for url in urls]
+print(f'{green("[~]")} Fetching Urls')
+
+for future in as_completed(futures):
+    [data,diff] = future.result()
+print(f'{green("[~]")} Storing Data And Trying To Find Difference')
+
+data = date_stamp(data)
+header = f'{data.get("date")[-1]} - {datetime.datetime.now().strftime("%x")}'
+update_file(data)
+print(f'{green("[~]")} Creating HTML File ...')
+
+# Constructing output.html with diff data
+properties = ["url","code","length", "lines", "words", "location"]
+for d in diff:
+    html += "<tr>"
+    for p in properties:
+        val = (d.get(p) or "No Change")
+        if val != "No Change" and p!="url":
+            html += f'<td class="highlight">{val}</td>'
+        else:
+            html += f'<td>{val}</td>'
+    html += "</tr>"
+
+if html:
+    output_save(html,header,output_file_name)
+print(f'{green("[~]")} Done.')
+
+
+print("--- %s seconds ---" % (time.time()-start_time))
---
+++ src/utitlis.py
@@ -0,0 +1,70 @@
+import base64,json,datetime
+import requests
+
+def collect(url,data,diff):
+    if url:
+        re = req(url)
+        if data.get(url):
+            [data,diff] = Compare_and_add(data,re,url,diff)
+        else:
+            # this adds the "re" object to the json file
+            data.update(re)
+    return [data,diff]
+
+# request url and create json object with different properties
+def req(u):
+    db,prop={},{}
+    rq = requests.get(u, allow_redirects=False)
+    prop['code'] = [rq.status_code]
+    prop['length'] = [len(rq.text)]
+    prop['lines'] = [len(rq.text.split('\n'))]
+    prop['words'] = [len(rq.text.split(' '))]
+    prop['location'] = [rq.headers.get('Location')]
+    db[rq.url] = prop
+    return db
+
+
+# read data.json
+def get_old_data(data={}):
+    with open('data.json', 'r') as json_file:
+        data = json.load(json_file)
+    return data
+
+# compare data[url] json with req[url], add to the data.json and diff
+def Compare_and_add(data,req,url,diff):
+    temp={}
+    for prop in data[url].keys():
+        old,new=data[url][prop],req[url][prop]
+        if old[-1]!=new[0]:
+            temp['url'] = url
+            if prop != "location" and prop != "code":
+                temp[prop] = abs(int(old[-1])-int(new[0]))
+            else:
+                temp[prop] = f'{(old[-1] or 0)} --> {(new[0] or 0)}'
+        data[url][prop].append(req[url][prop][0])
+    if temp!={} and temp.get('url'): diff.append(temp)
+    return [data,diff]
+
+# update the data.json with new date
+def update_file(data):
+    with open("data.json", "w") as jsonFile:
+        json.dump(data, jsonFile)
+
+# create `output.html` from `template.html` and `diff data`
+def output_save(html,header,name="./output.html"):
+    with open('src/template.html', 'r') as template:
+        result = template.read()
+        result = result.replace("FILL_WITH_DIFF_RESULT",html)
+        result = result.replace("HEADER",header)
+    f = open(name,"w")
+    f.write(result)
+    f.close()
+
+# Add today's date in data.json
+def date_stamp(data):
+    today=datetime.datetime.now().strftime("%x")
+    if data.get('date'):
+        data['date'].append(today)
+    else:
+        data['date'] = [today]
+    return data
